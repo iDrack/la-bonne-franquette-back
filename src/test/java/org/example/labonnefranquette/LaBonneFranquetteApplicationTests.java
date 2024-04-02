8aaf7e2da4fcd9b513b66a120d4e6e6871067927
iDrack <tdignoire@protonmail.com>
2024-04-02 20:55:39 +0200

    Init project

---
+++ src/test/java/org/example/labonnefranquette/LaBonneFranquetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.labonnefranquette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBonneFranquetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
