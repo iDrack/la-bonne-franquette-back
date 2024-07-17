c411339ac9d5c71bf59e8c8fe3a001aa7d32ccc6
Thomas Dignoire <tdignoire@protonmail.com>
2024-07-17 11:31:52 +0200

    :white_check_mark: Ajout des tests sur le reste des models

---
+++ src/test/java/org/labonnefranquette/data/model/ExtraTest.java
@@ -0,0 +1,36 @@
+package org.labonnefranquette.data.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ExtraTest {
+
+    private Extra extra;
+
+    @BeforeEach
+    public void setUp() {
+        extra = new Extra();
+    }
+
+    @Test
+    public void setPrixHT_setsPrixHTCorrectly() {
+        extra.setPrixHT(100);
+        assertEquals(100, extra.getPrixHT());
+    }
+
+    @Test
+    public void setIngredient_setsIngredientCorrectly() {
+        Ingredient ingredient = new Ingredient();
+        extra.setIngredient(ingredient);
+        assertEquals(ingredient, extra.getIngredient());
+    }
+
+    @Test
+    public void setIngredient_setsNullIngredient() {
+        extra.setIngredient(null);
+        assertNull(extra.getIngredient());
+    }
+}
