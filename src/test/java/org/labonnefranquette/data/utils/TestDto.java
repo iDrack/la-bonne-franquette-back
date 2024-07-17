4c06684a7de1d3ccd3f8087abff3a41efd63330f
Thomas Dignoire <tdignoire@protonmail.com>
2024-07-17 12:31:51 +0200

    :white_check_mark: Ajout des tests sur les utils

---
+++ src/test/java/org/labonnefranquette/data/utils/TestDto.java
@@ -0,0 +1,11 @@
+package org.labonnefranquette.data.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestDto {
+    private int id;
+    private String name;
+}
---
+++ src/test/java/org/labonnefranquette/data/utils/TestEntity.java
@@ -0,0 +1,13 @@
+package org.labonnefranquette.data.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TestEntity {
+    // getters and setters
+    private int id;
+    private String name;
+
+}
