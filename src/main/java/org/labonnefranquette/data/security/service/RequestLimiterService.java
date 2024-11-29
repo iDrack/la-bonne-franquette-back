a5b21ed26093550eafbcce7eb1417b42342dc05d
François K <francoiska_fr@icloud.com>
2024-11-29 21:33:02 +0100

    🔇 Cron chaque jour pour daily clear

--- src/main/java/org/labonnefranquette/data/security/service/RequestLimiterService.java
+++ src/main/java/org/labonnefranquette/data/security/service/RequestLimiterService.java
@@ -8,7 +8,9 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class RequestLimiterService {
+
     private final Map<String, Long> requestTimes = new ConcurrentHashMap<>();
+    private volatile long lastRequestTime = System.currentTimeMillis();
 
     public boolean isRequestAllowed(HttpServletRequest request) {
         String clientIp = request.getRemoteAddr();
@@ -16,6 +18,7 @@ public class RequestLimiterService {
         String key = clientIp + ":" + endpoint;
 
         long currentTime = System.currentTimeMillis();
+        lastRequestTime = currentTime;
         long lastRequestTime = requestTimes.getOrDefault(key, 0L);
 
         if (currentTime - lastRequestTime < TimeUnit.SECONDS.toMillis(2)) {
@@ -24,5 +27,13 @@ public class RequestLimiterService {
 
         requestTimes.put(key, currentTime);
         return true;
+    }
+
+    public void clearRequestTimes() {
+        requestTimes.clear();
+    }
+
+    public long getLastRequestTime() {
+        return lastRequestTime;
     }
 }
