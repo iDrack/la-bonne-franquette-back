35bb4f365f926acb7b772fa75f42cad7fa74e867
Thomas Dignoire <tdignoire@protonmail.com>
2025-01-18 20:45:43 +0100

    ⚡️ Modification de la récupération des Optionals

--- src/main/java/org/labonnefranquette/data/services/CommandeService.java
+++ src/main/java/org/labonnefranquette/data/services/CommandeService.java
@@ -6,14 +6,14 @@ import org.labonnefranquette.data.model.enums.StatusCommande;
 import org.labonnefranquette.data.projection.CommandeListeProjection;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommandeService {
 
     List<Commande> findAllCommande();
     List<Commande> findAllCommandeWithStatut(StatusCommande status);
     List<CommandeListeProjection> findAllCommandeListe();
-    Optional<Commande> findCommandeById(long id);
+
+    Commande findCommandeById(long id);
     Commande createCommande(Commande commande);
 
     Boolean deleteCommande(Long id);
--- src/main/java/org/labonnefranquette/data/services/PaiementService.java
+++ src/main/java/org/labonnefranquette/data/services/PaiementService.java
@@ -4,12 +4,11 @@ import org.labonnefranquette.data.dto.impl.PaiementCreateDTO;
 import org.labonnefranquette.data.model.Paiement;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PaiementService {
     List<Paiement> getAllPaiement();
 
-    Optional<Paiement> getPaiementById(Long id);
+    Paiement getPaiementById(Long id);
 
     Paiement createPaiement(Long idCommande, PaiementCreateDTO paiementCreateDTO) throws RuntimeException;
 
