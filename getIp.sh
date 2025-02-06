d1934e6314d204d5a9b07efca4f43f3f527a00e6
Thomas Dignoire <tdignoire@protonmail.com>
2025-02-06 16:50:33 +0100

    :bug: Correction de la génération d'ip

--- getIp.sh
+++ getIp.sh
@@ -2,6 +2,6 @@
 
 PORT=$(grep '^PORT=' .env | cut -d '=' -f2 | tr -d '"')
 IP_ADDR=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1)
-IP_PORT="http://${IP_ADDR}:${PORT}"
-echo $IP_PORT | qrencode -s 9 -l H -o "ipaddr-QR.png"
+IP_PORT="${IP_ADDR}:${PORT}"
+echo -n $IP_PORT | qrencode -s 9 -l H -o "ipaddr-QR.png"
 sxiv ipaddr-QR.png
